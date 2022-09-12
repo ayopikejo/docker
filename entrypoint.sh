abeae6d21986cb21bda525aeb82adbff738c7606
Przemek Kwasniewicz <48388174+ayopikejo@users.noreply.github.com>
2022-09-12 20:42:06 +0200

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# DedicatedServerUtils/Setup_tModLoaderServer.sh
+
+
+input="install.txt"
+if [ -f "$input" ] ; then
+	str="+force_install_dir /root/terraria-server/workshop-mods +login anonymous"
+	while read -r line
+	do
+		str="$str +workshop_download_item 1281930 $line",
+	done < "$input"
+	str="$str +quit"
+	
+	steamcmd $str
+fi
+
+./start-tModLoaderServer.sh -serverconfig.txt -steamworkshopfolder /root/terraria-server/workshop-mods/steamapps/workshop
